d5fcf614fccb3b81983d3acf462c0c574d262a5c
kokikudo <massanmesu@icloud.com>
2022-01-14 17:23:11 +0900

    create ScrollView

--- app/src/main/java/com/example/viewpager2_tablayout_scrollview/MainActivity.kt
+++ app/src/main/java/com/example/viewpager2_tablayout_scrollview/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.viewpager2_tablayout_scrollview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+    スクロールする画面を作りたいときはレイアウト新規作成時にRoot elementをScrollViewにする
+    ScrollViewのは以下にLinerLayoutなどの要素を配置する
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
